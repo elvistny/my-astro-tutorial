c5cabf33db7b614a5d5d21a4c28090af6737e400
Elvis <elvistny@gmail.com>
2026-03-24 15:05:52 +0800

    6-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://luxury-starship-fa9387.netlify.app/"
+  site: "https://luxury-starship-fa9387.netlify.app/",
+  integrations: [preact()]
 });
