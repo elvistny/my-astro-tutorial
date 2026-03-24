9decafb1843be1d5cae85e69552126ee9f9abfed
Elvis <elvistny@gmail.com>
2026-03-24 14:46:26 +0800

    5-4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://luxury-starship-fa9387.netlify.app/"
+});
